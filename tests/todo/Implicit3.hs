8f516650610e9e501300d64890bc7c0bee803162
Anish Tondwalkar <atondwal@ucsd.edu>
2018-04-27 16:50:55 -0700

    Split todo/Implicit2.hs into minimal example
    The fq generated from it with `liquid --save` can be checked save
    by fixpoint, but crashes when called by LH itself!

--- tests/todo/Implicit3.hs
+++ tests/todo/Implicit3.hs
@@ -1,9 +1,12 @@
+
 {-@ type IntN N = {v:Int | v = N} @-}
 
 {-@ foo :: n:Int ~> (() -> IntN n) -> IntN {n+1} @-}
 foo :: (() -> Int) -> Int
 foo f = 1 + f ()
 
-{-@ test3 :: m:Int -> IntN {m+2} @-}
-test3 m = foo (\_ -> m+1)
+{-@ test2 :: m:Int -> IntN {m+1} @-}
+test2 m = foo (\_ -> m)
 
+{-@ bar :: IntN 3 @-}
+bar = foo (\_ -> foo (\_ -> 1))
